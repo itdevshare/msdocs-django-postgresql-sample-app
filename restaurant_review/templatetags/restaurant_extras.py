d081b6fd6b86265c3aac986acbbec5db58c645d1
Andrey <akiselev@fts-eu.com>
2023-04-27 13:04:22 +0300

    rating

--- restaurant_review/templatetags/restaurant_extras.py
+++ restaurant_review/templatetags/restaurant_extras.py
@@ -5,5 +5,6 @@ register = template.Library()
 
 @register.inclusion_tag('restaurant_review/star_rating.html')
 def star_rating(avg_rating, review_count):
-    stars_percent = round((avg_rating / 5.0) * 100) if review_count > 0 else 0
+    #stars_percent = round((avg_rating / 5.0) * 100) if review_count > 0 else 0
+    stars_percent = round((avg_rating / 5.0) * 100) if review_count > 0 else 10
     return {'avg_rating': avg_rating, 'review_count': review_count, 'stars_percent': stars_percent}
